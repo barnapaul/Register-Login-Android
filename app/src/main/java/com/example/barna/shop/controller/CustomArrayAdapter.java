0d3d99ca738f2c5e3a8d649136ec7594f9a4ad61
barnapaul <paul.barna@emanuel.ro>
2017-11-13 16:45:36 +0200

    step 1 xml to java userScreen

--- app/src/main/java/com/example/barna/shop/controller/CustomArrayAdapter.java
+++ app/src/main/java/com/example/barna/shop/controller/CustomArrayAdapter.java
@@ -23,15 +23,12 @@ public class CustomArrayAdapter extends ArrayAdapter<Person> {
 
     Activity activity;
 
-
-    public CustomArrayAdapter(Context context,  ArrayList<Person> users, Activity activity) {
+    public CustomArrayAdapter(Context context,  ArrayList<Person> users) {
         super(context, R.layout.row, users);
         layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         usersList = users;
-        this.activity = activity;
 
     }
-
 
 
     @Override
@@ -46,85 +43,103 @@ public class CustomArrayAdapter extends ArrayAdapter<Person> {
 
     @Override
     public @NonNull View  getView(int position, View convertView,@NonNull ViewGroup parent) {
-        ViewHolder holder;
+//        ViewHolder holder;
 
         if (convertView == null) {
 
             convertView = layoutInflater.inflate(R.layout.row, parent, false);
 
-            holder = new ViewHolder(convertView);
+//            holder = new ViewHolder(convertView);
 
+//            convertView.setTag(holder);
 
-            LinearLayout linearLayout = (LinearLayout) activity.findViewById(R.id.linearLayout);
-
-
-            TextView textView = new TextView(getContext());
-
-//            textView.setLayoutParams(new LinearLayout.LayoutParams());
-
-            ((LinearLayout) linearLayout).addView(textView);
-
-
-
-
-
-            convertView.setTag(holder);
-
-        } else {
-            holder = (ViewHolder) convertView.getTag();
         }
+// else {
+//            holder = (ViewHolder) convertView.getTag();
+//        }
+
+//        LinearLayout linearLayout  = (LinearLayout) activity.findViewById(R.id.linearLayout);
 
 
 
+        LinearLayout linearLayout = (LinearLayout) convertView;
         Person p = usersList.get(position);
-        String typeStr = (getContext().getString(R.string.type) + " " + p.getType());
-        String firstNameStr =getContext().getString (R.string.first_name) + ": " + p.getFirstName();
-        String lastNameStr = getContext().getString (R.string.last_name) + ": " + p.getLastName();
-        String emailStr = getContext().getString (R.string.email) + ": " + p.getEmail();
-        String phoneStr = getContext().getString (R.string.phone) + ": " + p.getPhone();
-        String passwordStr =  getContext().getString (R.string.password) + ": " + p.getPassword();
-        String facultyStr =  getContext().getString (R.string.faculty) + ": " + p.getFaculty();
-        String yearStr =  getContext().getString (R.string.year) + ": " + p.getYear();
 
-        holder.userType.setText(typeStr);
-        holder.firstName.setText(firstNameStr);
-        holder.lastName.setText(lastNameStr);
-        holder.email.setText(emailStr);
-        holder.phone.setText(phoneStr);
-        holder.password.setText(passwordStr);
-        holder.faculty.setText("");
-        holder.year.setText("");
-        if (p.getType().equals("Student")) {
-            holder.faculty.setText(facultyStr);
-            holder.year.setText(yearStr);
+
+        TextView[]textViews =  new TextView[7];
+
+
+        for( int i = 0; i < 7; i++ )
+        {
+           textViews[i] = new TextView(getContext());
+           textViews[i].setLayoutParams(new LinearLayout.LayoutParams(
+                   ViewGroup.LayoutParams.WRAP_CONTENT,
+                   ViewGroup.LayoutParams.MATCH_PARENT));
+           textViews[i].setText(i);
+
+           linearLayout.addView(textViews[i]);
         }
+
+
+
+
+//        Person p = usersList.get(position);
+//        String typeStr = (getContext().getString(R.string.type) + " " + p.getType());
+//        String firstNameStr =getContext().getString (R.string.first_name) + ": " + p.getFirstName();
+//        String lastNameStr = getContext().getString (R.string.last_name) + ": " + p.getLastName();
+//        String emailStr = getContext().getString (R.string.email) + ": " + p.getEmail();
+//        String phoneStr = getContext().getString (R.string.phone) + ": " + p.getPhone();
+//        String passwordStr =  getContext().getString (R.string.password) + ": " + p.getPassword();
+//        String facultyStr =  getContext().getString (R.string.faculty) + ": " + p.getFaculty();
+//        String yearStr =  getContext().getString (R.string.year) + ": " + p.getYear();
+//
+//        holder.userType.setText(typeStr);
+//        holder.firstName.setText(firstNameStr);
+//        holder.lastName.setText(lastNameStr);
+//        holder.email.setText(emailStr);
+//        holder.phone.setText(phoneStr);
+//        holder.password.setText(passwordStr);
+//        holder.faculty.setText("");
+//        holder.year.setText("");
+//        if (p.getType().equals("Student")) {
+//            holder.faculty.setText(facultyStr);
+//            holder.year.setText(yearStr);
+//        }
 
         return convertView;
 
     }
 
-    private static class ViewHolder {
-        private TextView userType;
-        private TextView firstName;
-        private TextView lastName;
-        private TextView email;
-        private TextView phone;
-        private TextView password;
-        private TextView faculty;
-        private TextView year;
-
-
-        public ViewHolder(View convertView) {
-
-            userType = (TextView) convertView.findViewById(R.id.userType);
-            firstName = (TextView) convertView.findViewById(R.id.firstName);
-            lastName = (TextView) convertView.findViewById(R.id.lastName);
-            email = (TextView) convertView.findViewById(R.id.email);
-            phone = (TextView) convertView.findViewById(R.id.phone);
-            password = (TextView) convertView.findViewById(R.id.password);
-            faculty = (TextView) convertView.findViewById(R.id.faculty);
-            year = (TextView) convertView.findViewById(R.id.year);
+//    private static class ViewHolder {
+//        private TextView userType;
+//        private TextView firstName;
+//        private TextView lastName;
+//        private TextView email;
+//        private TextView phone;
+//        private TextView password;
+//        private TextView faculty;
+//        private TextView year;
+//
+//
+//        private ViewHolder(View convertView) {
+//
+//            userType = (TextView) convertView.findViewById(R.id.userType);
+//            firstName = (TextView) convertView.findViewById(R.id.firstName);
+//            lastName = (TextView) convertView.findViewById(R.id.lastName);
+//            email = (TextView) convertView.findViewById(R.id.email);
+//            phone = (TextView) convertView.findViewById(R.id.phone);
+//            password = (TextView) convertView.findViewById(R.id.password);
+//            faculty = (TextView) convertView.findViewById(R.id.faculty);
+//            year = (TextView) convertView.findViewById(R.id.year);
+//        }
+//
+//    }
+    public void takeField(int i,String k){
+        if (i==1){
+            k=getContext().getString(R.string.first_name);
         }
+    }
 
     }
-}
+
+
